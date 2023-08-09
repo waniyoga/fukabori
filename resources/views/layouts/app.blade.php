88e5f31bb4ead797c5af1a71b8edc903c8c09205
waniyoga <130149695+waniyoga@users.noreply.github.com>
2023-08-09 17:29:06 +0900

    ログイン画面戻し

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,11 +18,11 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <style>
+    {{-- <style>
         .navbar-collapse {
             display: block!important;
         }
-    </style>
+    </style> --}}
 </head>
 <body>
     <div id="app">
@@ -31,9 +31,9 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
-                </button> --}}
+                </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
